400894b42f46e6e459e98419f9325dcc436da562
Jan Klimaschewski <jankxski@gmail.com>
2022-09-14 20:14:57 +0200

    tikz export v0

--- Source/ConcordEditor/Private/ConcordModelGraphTikzExporter.cpp
+++ Source/ConcordEditor/Private/ConcordModelGraphTikzExporter.cpp
@@ -12,10 +12,11 @@
 #include "Widgets/Input/SButton.h"
 #include "Misc/FileHelper.h"
 #include "HAL/PlatformProcess.h"
+#include "ConcordVertex.h"
 
 void UConcordModelGraphTikzExporter::Export(const UConcordModelGraph* Graph)
 {
-	UConcordModelGraphTikzExporter* Exporter = FindObjectFast<UConcordModelGraphTikzExporter>(GetTransientPackage(), "ConcordModelGraphTikzExporter");
+    UConcordModelGraphTikzExporter* Exporter = FindObjectFast<UConcordModelGraphTikzExporter>(GetTransientPackage(), "ConcordModelGraphTikzExporter");
     if (!Exporter)
     {
         Exporter = NewObject<UConcordModelGraphTikzExporter>(GetTransientPackage(), "ConcordModelGraphTikzExporter");
@@ -69,17 +70,197 @@ void UConcordModelGraphTikzExporter::Export(const UConcordModelGraph* Graph)
 
     FSlateApplication::Get().AddModalWindow(Window, ParentWindow, false);
 
-	Exporter->ExportInteral(Graph);
+    Exporter->ExportInteral(Graph);
 }
 
 UConcordModelGraphTikzExporter::UConcordModelGraphTikzExporter()
-	: Directory({ FPlatformProcess::UserDir() })
+    : Directory({ FPlatformProcess::UserDir() })
     , FileName("Graph.tikz")
-	, DistanceScale(1.0f)
+    , DistanceScale(0.0f)
 {}
 
 void UConcordModelGraphTikzExporter::ExportInteral(const UConcordModelGraph* Graph)
 {
-	FFileHelper::SaveStringToFile(FString("FooBar"), *(Directory.Path / FileName));
-	// TODO
+    Buffer.Empty(1 << 15);
+    Center = GetCenter(Graph);
+    WriteHeader();
+    for (const UEdGraphNode* Node : Graph->Nodes) WriteNode(Node);
+    for (const UEdGraphNode* Node : Graph->Nodes) WriteOutgoingEdges(Node);
+    WriteFooter();
+    FFileHelper::SaveStringToFile(Buffer, *(Directory.Path / FileName), FFileHelper::EEncodingOptions::ForceUTF8WithoutBOM);
+}
+
+FVector2D UConcordModelGraphTikzExporter::GetCenter(const UEdGraph* Graph)
+{
+    FVector2D Center = { 0.0, 0.0 };
+    for (const UEdGraphNode* Node : Graph->Nodes)
+    {
+        Center.X += Node->NodePosX;
+        Center.Y += Node->NodePosY;
+    }
+    Center /= Graph->Nodes.Num();
+    return Center;
+}
+
+void UConcordModelGraphTikzExporter::WriteHeader()
+{
+    Buffer += TEXT(R"_(\usetikzlibrary{fit, positioning}
+\begin{tikzpicture}[every matrix/.style={column sep = 0.2cm}]
+    \tikzstyle{pin} = [above = -0.09cm, fill, circle, minimum size = 0.2cm, inner sep = 0cm]
+    \tikzstyle{pinlabel} = [font = \footnotesize, minimum width = 0.5cm]
+    \tikzstyle{inlabel} = [pinlabel, right = 0.1cm, anchor = mid west]
+    \tikzstyle{outlabel} = [pinlabel, left = 0.1cm, anchor = mid east]
+    \tikzstyle{nodetitle} = [minimum height = 0.7cm]
+    \tikzstyle{nodedetails} = [font = \footnotesize, text = gray]
+)_");
+}
+
+void UConcordModelGraphTikzExporter::WriteNode(const UEdGraphNode* Node)
+{
+    double X = (Node->NodePosX - Center.X) * 0.02 * FMath::Pow(2.0, DistanceScale);
+    double Y = (Center.Y - Node->NodePosY) * 0.02 * FMath::Pow(2.0, DistanceScale);
+    Buffer += TEXT("    \\matrix(");
+    WriteID(Node, TEXT("matrix"));
+    Buffer += TEXT(") at ");
+    Buffer += FString::Printf(TEXT("(%.3f, %.3f)"), X, Y);
+    Buffer += TEXT("\n    {\n");
+    WriteInputOutputPinPairs(Node);
+    Buffer += TEXT("    };\n");
+
+    Buffer += TEXT("    \\node[nodetitle, above = 0cm of ");
+    WriteID(Node, TEXT("matrix"));
+    Buffer += TEXT(".north](");
+    WriteID(Node, TEXT("title"));
+    Buffer += TEXT("){");
+    WriteTitle(Node);
+    Buffer += TEXT("};\n");
+
+    Buffer += TEXT("    \\matrix[below = 0cm of ");
+    WriteID(Node, TEXT("matrix"));
+    Buffer += TEXT(".south](");
+    WriteID(Node, TEXT("details"));
+    Buffer += TEXT(")\n    {\n");
+    WriteDetails(Node);
+    Buffer += TEXT("    };\n");
+
+    Buffer += TEXT("    \\node[draw, rounded corners, fit = (");
+    WriteID(Node, TEXT("matrix"));
+    Buffer += TEXT(") (");
+    WriteID(Node, TEXT("title"));
+    Buffer += TEXT(")] {};\n");
+
+    WriteDefaultValues(Node);
+}
+
+void UConcordModelGraphTikzExporter::WriteOutgoingEdges(const UEdGraphNode* Node)
+{
+    for (const UEdGraphPin* Pin : Node->Pins)
+    {
+        if (Pin->Direction == EGPD_Input) continue;
+        for (const UEdGraphPin* ConnectedPin : Pin->LinkedTo)
+        {
+            Buffer += TEXT("    \\draw (");
+            WritePinID(Pin);
+            Buffer += TEXT(") to [out = 0, in = 180] (");
+            WritePinID(ConnectedPin);
+            Buffer += TEXT(");\n");
+        }
+    }
+}
+
+void UConcordModelGraphTikzExporter::WriteDefaultValues(const UEdGraphNode* Node)
+{
+    for (const UEdGraphPin* Pin : Node->Pins)
+    {
+        if (Pin->Direction == EGPD_Output) continue;
+        if (!Pin->LinkedTo.IsEmpty()) continue;
+        const FString DefaultString = Pin->GetDefaultAsString();
+        if (DefaultString.IsEmpty()) continue;
+        Buffer += TEXT("    \\node [left = 0.5cm of ");
+        WritePinID(Pin);
+        Buffer += TEXT("] (");
+        WritePinID(Pin, TEXT("default"));
+        Buffer += TEXT(") {");
+        Buffer += DefaultString;
+        Buffer += TEXT("};\n");
+
+        Buffer += TEXT("    \\draw (");
+        WritePinID(Pin);
+        Buffer += TEXT(") -- (");
+        WritePinID(Pin, TEXT("default"));
+        Buffer += TEXT(");\n");
+    }
+}
+
+void UConcordModelGraphTikzExporter::WriteFooter()
+{
+    Buffer += TEXT("\\end{tikzpicture}");
+}
+
+void UConcordModelGraphTikzExporter::WriteID(const UEdGraphNode* Node, const TCHAR* Suffix, TOptional<EEdGraphPinDirection> PinDirection)
+{
+    Buffer += Node->GetName();
+    if (PinDirection)
+    {
+        if (PinDirection.GetValue() == EGPD_Input) Buffer += TEXT("__TIKZ_INPIN__");
+        else Buffer += TEXT("__TIKZ_OUTPIN__");
+    }
+    else Buffer += TEXT("__TIKZ__");
+    Buffer += Suffix;
+}
+
+void UConcordModelGraphTikzExporter::WritePinID(const UEdGraphPin* Pin, const TCHAR* Suffix)
+{
+    int32 PinIndex = 0;
+    Pin->GetOwningNode()->Pins.Find(const_cast<UEdGraphPin*>(Pin), PinIndex);
+    WriteID(Pin->GetOwningNode(), *FString::Printf(TEXT("%d"), PinIndex), Pin->Direction.GetValue());
+    Buffer += Suffix;
+}
+
+void UConcordModelGraphTikzExporter::WriteTitle(const UEdGraphNode* Node)
+{
+    Buffer += Node->GetNodeTitle(ENodeTitleType::FullTitle).ToString();
+}
+
+void UConcordModelGraphTikzExporter::WriteInputOutputPinPairs(const UEdGraphNode* Node, int32 InputPinIndex, int32 OutputPinIndex)
+{
+    while (InputPinIndex < Node->Pins.Num() && Node->Pins[InputPinIndex]->Direction == EGPD_Output) ++InputPinIndex;
+    while (OutputPinIndex < Node->Pins.Num() && Node->Pins[OutputPinIndex]->Direction == EGPD_Input) ++OutputPinIndex;
+    if (InputPinIndex >= Node->Pins.Num() && OutputPinIndex >= Node->Pins.Num()) return;
+    Buffer += TEXT("        ");
+    if (InputPinIndex < Node->Pins.Num()) WritePin(Node->Pins[InputPinIndex++]);
+    else WritePinPlaceholder(EGPD_Input);
+    Buffer += TEXT(" & ");
+    if (OutputPinIndex < Node->Pins.Num()) WritePin(Node->Pins[OutputPinIndex++]);
+    else WritePinPlaceholder(EGPD_Output);
+    Buffer += TEXT(" \\\\\n");
+    WriteInputOutputPinPairs(Node, InputPinIndex, OutputPinIndex);
+}
+
+void UConcordModelGraphTikzExporter::WritePin(const UEdGraphPin* Pin)
+{
+    Buffer += TEXT("\\node [pin] (");
+    WritePinID(Pin);
+    Buffer += TEXT(") {}; \\node [");
+    if (Pin->Direction == EGPD_Input) Buffer += TEXT("inlabel");
+    else Buffer += TEXT("outlabel");
+    Buffer += TEXT("] {");
+    if (!Pin->Direction == EGPD_Output || !Pin->GetOwningNode()->IsA<UConcordModelGraphTransformer>())
+        Buffer += Pin->GetOwningNode()->GetPinDisplayName(Pin).ToString();
+    Buffer += TEXT("};");
+}
+
+void UConcordModelGraphTikzExporter::WritePinPlaceholder(EEdGraphPinDirection Direction)
+{
+    Buffer += TEXT("\\node [");
+    if (Direction == EGPD_Input) Buffer += TEXT("inlabel");
+    else Buffer += TEXT("outlabel");
+    Buffer += TEXT("] {}; ");
+}
+
+void UConcordModelGraphTikzExporter::WriteDetails(const UEdGraphNode* Node)
+{
+    Buffer += TEXT("        \\node [nodedetails] {Shape: ");
+    Buffer += ConcordShape::ToString(Cast<UConcordModelGraphNode>(Node)->Vertex->GetShape());
+    Buffer += TEXT("}; \\\\\n");
 }
--- Source/ConcordEditor/Private/ConcordModelGraphTikzExporter.h
+++ Source/ConcordEditor/Private/ConcordModelGraphTikzExporter.h
@@ -6,6 +6,8 @@
 #include "ConcordModelGraphTikzExporter.generated.h"
 
 class UConcordModelGraph;
+class UConcordModelGraphNode;
+class UEdGraph;
 
 UCLASS()
 class UConcordModelGraphTikzExporter : public UObject
@@ -22,8 +24,27 @@ public:
     UPROPERTY(EditAnywhere)
     FString FileName;
 
-    UPROPERTY(EditAnywhere, meta = (ClampMin = 0, ClampMax = 1))
+    UPROPERTY(EditAnywhere, meta = (ClampMin = -5, ClampMax = 5))
     float DistanceScale;
+
+    static FVector2D GetCenter(const UEdGraph* Graph);
 private:
     void ExportInteral(const UConcordModelGraph* Graph);
+
+    void WriteHeader();
+    void WriteNode(const UEdGraphNode* Node);
+    void WriteOutgoingEdges(const UEdGraphNode* Node);
+    void WriteDefaultValues(const UEdGraphNode* Node);
+    void WriteFooter();
+
+    void WriteID(const UEdGraphNode* Node, const TCHAR* Suffix, TOptional<EEdGraphPinDirection> PinDirection = {});
+    void WritePinID(const UEdGraphPin* Pin, const TCHAR* Suffix = TEXT(""));
+    void WriteTitle(const UEdGraphNode* Node);
+    void WriteInputOutputPinPairs(const UEdGraphNode* Node, int32 InputPinIndex = 0, int32 OutputPinIndex = 0);
+    void WritePin(const UEdGraphPin* Pin);
+    void WritePinPlaceholder(EEdGraphPinDirection Direction);
+    void WriteDetails(const UEdGraphNode* Node);
+
+    FString Buffer;
+    FVector2D Center;
 };
