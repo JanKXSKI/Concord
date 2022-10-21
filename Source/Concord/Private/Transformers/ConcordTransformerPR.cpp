f9b2ccd51c12c7c1719e64b04d331a5bd2f58080
Jan Klimaschewski <jankxski@gmail.com>
2022-10-21 12:48:17 +0200

    GTTM Preference Rule Transformer Structure

--- Source/Concord/Private/Transformers/ConcordTransformerPR.cpp
+++ Source/Concord/Private/Transformers/ConcordTransformerPR.cpp
@@ -2,32 +2,29 @@
 
 #include "Transformers/ConcordTransformerPR.h"
 
-int32 FConcordPRExpression::NumNotes() const
+int32 FConcordGPR1Expression::ComputeScoreClass(const FConcordExpressionContext<float>& Context) const
 {
-    return SourceExpressions.Num() - 1;
+    int32 NumNotes = 0;
+    const auto F = [&](int32 Degree){ if (Degree > 0) ++NumNotes; return NumNotes > 2; };
+    if (int32 Index = 0; StepForward(F, Index, Context)) return 0;
+    switch (NumNotes)
+    {
+    case 0: return -4;
+    case 1: return -2;
+    default: return -1;
+    }
 }
 
-int32 FConcordPRExpression::GetNoteBoundaryIndex(const FConcordExpressionContext<float>& Context) const
+int32 FConcordGPR2bExpression::ComputeScoreClass(const FConcordExpressionContext<float>& Context) const
 {
-    return SourceExpressions[0]->ComputeValue(Context).Int;
-}
-
-int32 FConcordPRExpression::GetNote(int32 Index, const FConcordExpressionContext<float>& Context) const
-{
-    check(Index >= 0 && Index < NumNotes());
-    return SourceExpressions[Index - 1]->ComputeValue(Context).Int;
-}
-
-FConcordValue FConcordGPR2bExpression::ComputeValue(const FConcordExpressionContext<float>& Context) const
-{
-    const int32 NoteBoundaryIndex = GetNoteBoundaryIndex(Context);
-    const int32 BoundaryNote = GetNote(NoteBoundaryIndex, Context);
-    if (BoundaryNote <= 0) return 0;
-    int32 StepIndex = NoteBoundaryIndex;
+    const int32 BoundaryIndex = GetBoundaryIndex(Context);
+    const int32 BoundaryDegree = GetDegree(BoundaryIndex, Context);
+    if (BoundaryDegree <= 0) return 0;
+    int32 StepIndex = BoundaryIndex;
     int32 Intervals[3] {};
-    if (StepBackwards([&](int32 Index){ ++Intervals[1]; return GetNote(Index, Context) > 0; }, StepIndex)) return 0;
-    if (StepBackwards([&](int32 Index){ ++Intervals[0]; return GetNote(Index, Context) > 0; }, StepIndex)) return 0;
-    if (StepForwards([&](int32 Index){ ++Intervals[2]; return GetNote(Index, Context) > 0; }, StepIndex = NoteBoundaryIndex)) return 0;
-    if (Intervals[1] < Intervals[0] || Intervals[1] < Intervals[2]) return 0;
+    if (StepBackward([&](int32 Degree){ ++Intervals[1]; return Degree <= 0; }, StepIndex, Context)) return 0;
+    if (StepBackward([&](int32 Degree){ ++Intervals[0]; return Degree <= 0; }, StepIndex, Context)) return 0;
+    if (StepForward([&](int32 Degree){ ++Intervals[2]; return Degree <= 0; }, StepIndex = BoundaryIndex, Context)) return 0;
+    if (Intervals[1] <= Intervals[0] || Intervals[1] <= Intervals[2]) return 0;
     return Intervals[1] - Intervals[0] + Intervals[1] - Intervals[2];
 }
--- Source/Concord/Public/Transformers/ConcordTransformerPR.h
+++ Source/Concord/Public/Transformers/ConcordTransformerPR.h
@@ -6,6 +6,7 @@
 #include "ConcordTransformer.h"
 #include "ConcordTransformerPR.generated.h"
 
+template<typename FImpl>
 class FConcordPRExpression : public FConcordComputingExpression
 {
 public:
@@ -13,45 +14,81 @@ public:
 #if WITH_EDITOR
     FString ToString() const override { return TEXT("PR ToString() not implemented."); }
 #endif
+    FConcordValue ComputeValue(const FConcordExpressionContext<float>& Context) const override
+    {
+        return SourceExpressions[0]->ComputeValue(Context).Float * static_cast<const FImpl*>(this)->ComputeScoreClass(Context);
+    }
 protected:
-    int32 NumNotes() const;
-    int32 GetNoteBoundaryIndex(const FConcordExpressionContext<float>& Context) const;
-    int32 GetNote(int32 Index, const FConcordExpressionContext<float>& Context) const;
+    int32 GetDegreesOffset() const { return 1 + FImpl::NumAdditionalSourceExpressions; }
+    int32 GetNumDegrees() const { return SourceExpressions.Num() - GetDegreesOffset(); }
+    int32 GetDegree(int32 Index, const FConcordExpressionContext<float>& Context) const
+    {
+        check(Index >= 0 && Index < GetNumDegrees());
+        return SourceExpressions[Index + GetDegreesOffset()]->ComputeValue(Context).Int;
+    }
 
     template<typename FF>
-    bool StepBackwards(FF F, int& Index) const
+    bool StepBackward(FF F, int& Index, const FConcordExpressionContext<float>& Context) const
     {
-        for (--Index; Index >= 0; --Index) if (F(Index)) return false;
+        for (--Index; Index >= 0; --Index) if (!F(GetDegree(Index, Context))) return false;
         return true;
     }
 
     template<typename FF>
-    bool StepForwards(FF F, int& Index) const
+    bool StepForward(FF F, int& Index, const FConcordExpressionContext<float>& Context) const
     {
-        for (++Index; Index < NumNotes(); ++Index) if (F(Index)) return false;
+        for (++Index; Index < GetNumDegrees(); ++Index) if (!F(GetDegree(Index, Context))) return false;
         return true;
     }
 };
 
-class FConcordGPR2bExpression : public FConcordPRExpression
+template<typename FImpl>
+class FConcordPR2GroupsExpression : public FConcordPRExpression<FConcordPR2GroupsExpression<FImpl>>
 {
 public:
-    using FConcordPRExpression::FConcordPRExpression;
-    FConcordValue ComputeValue(const FConcordExpressionContext<float>& Context) const override;
+    using FBase = FConcordPRExpression<FConcordPR2GroupsExpression<FImpl>>;
+    using FBase::FBase;
+    static inline int NumAdditionalSourceExpressions = 1;
+    int32 ComputeScoreClass(const FConcordExpressionContext<float>& Context) const
+    {
+        return static_cast<const FImpl*>(this)->ComputeScoreClass(Context);
+    }
+protected:
+    int32 GetBoundaryIndex(const FConcordExpressionContext<float>& Context) const
+    {
+        return FBase::SourceExpressions[1]->ComputeValue(Context).Int;
+    }
 };
 
-UCLASS()
+class FConcordGPR1Expression : public FConcordPRExpression<FConcordGPR1Expression>
+{
+public:
+    using FBase = FConcordPRExpression<FConcordGPR1Expression>;
+    using FBase::FBase;
+    static inline int NumAdditionalSourceExpressions = 0;
+    int32 ComputeScoreClass(const FConcordExpressionContext<float>& Context) const;
+};
+
+class FConcordGPR2bExpression : public FConcordPR2GroupsExpression<FConcordGPR2bExpression>
+{
+public:
+    using FBase = FConcordPR2GroupsExpression<FConcordGPR2bExpression>;
+    using FBase::FBase;
+    int32 ComputeScoreClass(const FConcordExpressionContext<float>& Context) const;
+};
+
+UCLASS(Abstract)
 class CONCORD_API UConcordTransformerPR : public UConcordTransformer
 {
     GENERATED_BODY()
 public:    
     TArray<FInputInfo> GetInputInfo() const override
     {
-        return
-        {
-            { {}, EConcordValueType::Int, INVTEXT("Notes"), "0" },
-            { {}, EConcordValueType::Int, INVTEXT("Boundary Note Index"), "0" }
-        };
+        TArray<FInputInfo> InputInfo;
+        InputInfo.Add({ {}, EConcordValueType::Int, INVTEXT("Degrees"), "0" });
+        InputInfo.Add({ {}, EConcordValueType::Float, INVTEXT("Scale"), "0" });
+        AddAdditionalInputs(InputInfo);
+        return InputInfo;
     }
 
     FString GetCategory() const override { return TEXT("GTTM"); }
@@ -59,24 +96,27 @@ public:
     FConcordSharedExpression GetExpression(const FConcordMultiIndex& InMultiIndex) const override
     {
         TArray<FConcordSharedExpression> SourceExpressions;
-        AddBroadcastedConnectedExpressions(SourceExpressions, 1, 1, InMultiIndex);
-        FConcordMultiIndex NotesIndex { InMultiIndex };
-        ConcordShape::Unbroadcast(NotesIndex, NotesShape);
-        for (NotesIndex.Add(0); NotesIndex.Last() < GetConnectedTransformers()[0]->GetShape().Last(); ++NotesIndex.Last())
-            SourceExpressions.Add(GetConnectedTransformers()[0]->GetExpression(NotesIndex));
+        AddBroadcastedConnectedExpressions(SourceExpressions, 1, GetConnectedTransformers().Num() - 1, InMultiIndex);
+        FConcordMultiIndex DegreesIndex { InMultiIndex };
+        ConcordShape::Unbroadcast(DegreesIndex, DegreesShape);
+        for (DegreesIndex.Add(0); DegreesIndex.Last() < GetConnectedTransformers()[0]->GetShape().Last(); ++DegreesIndex.Last())
+            SourceExpressions.Add(GetConnectedTransformers()[0]->GetExpression(DegreesIndex));
         return MakeExpression(MoveTemp(SourceExpressions));
     }
 private:
     FSetupResult Setup(TOptional<FString>& OutErrorMessage) override
     {
-        NotesShape = GetConnectedTransformers()[0]->GetShape();
-        NotesShape.Pop(false);
-        TOptional<FConcordShape> BroadcastedShape = ConcordShape::Broadcast({ NotesShape, GetConnectedTransformers()[1]->GetShape() });
+        TArray<FConcordShape> ConnectedShapes = GetConnectedShapes();
+        ConnectedShapes[0].Pop(false);
+        DegreesShape = ConnectedShapes[0];
+        TOptional<FConcordShape> BroadcastedShape = ConcordShape::Broadcast(ConnectedShapes);
         if (!BroadcastedShape) { OutErrorMessage = TEXT("Input shapes could not be broadcasted together."); return {}; }
-        return { BroadcastedShape.GetValue(), EConcordValueType::Int };
+        return { BroadcastedShape.GetValue(), EConcordValueType::Float };
     }
 
-    FConcordShape NotesShape;
+    FConcordShape DegreesShape;
+
+    virtual void AddAdditionalInputs(TArray<FInputInfo>& InOutInputInfo) const {}
 
     virtual FConcordSharedExpression MakeExpression(TArray<FConcordSharedExpression>&& SourceExpressions) const
     {
@@ -86,13 +126,32 @@ private:
 };
 
 UCLASS()
+class CONCORD_API UConcordTransformerGPR1 : public UConcordTransformerPR
+{
+    GENERATED_BODY()
+public:
+    FText GetDisplayName() const override { return INVTEXT("GPR1"); }
+    FText GetTooltip() const override { return INVTEXT("Tries to apply grouping preference rule 1 to the input degrees."); }
+private:
+    FConcordSharedExpression MakeExpression(TArray<FConcordSharedExpression>&& SourceExpressions) const override
+    {
+        return MakeShared<const FConcordGPR1Expression>(MoveTemp(SourceExpressions));
+    }
+};
+
+UCLASS()
 class CONCORD_API UConcordTransformerGPR2b : public UConcordTransformerPR
 {
     GENERATED_BODY()
 public:
     FText GetDisplayName() const override { return INVTEXT("GPR2b"); }
-    FText GetTooltip() const override { return INVTEXT("Tries to apply grouping preference rule 2b before the boundary note."); }
+    FText GetTooltip() const override { return INVTEXT("Tries to apply grouping preference rule 2b before the boundary note given in scale degrees."); }
 private:
+    virtual void AddAdditionalInputs(TArray<FInputInfo>& InOutInputInfo) const
+    {
+        InOutInputInfo.Add({ {}, EConcordValueType::Int, INVTEXT("Boundary Degree Index"), "0" });
+    }
+
     FConcordSharedExpression MakeExpression(TArray<FConcordSharedExpression>&& SourceExpressions) const override
     {
         return MakeShared<const FConcordGPR2bExpression>(MoveTemp(SourceExpressions));
